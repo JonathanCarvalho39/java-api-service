d1c053a5fc0ccbed1820aafd2b6ec6fb7579aff1
JonathanCarvalho039 <jonathan.carvalho@sptech.school>
2024-06-13 11:46:59 -0300

    inicio-projeto

---
+++ src/main/java/br/com/erudio/apijavaservice/ApiJavaServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.apijavaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiJavaServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/erudio/apijavaservice/ApiJavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erudio.apijavaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
