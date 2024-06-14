b4fc55cbdfe963106eb8817854a9eee548ffb228
JonathanCarvalho39 <jonathan.carvalho@sptech.school>
2024-06-13 23:13:35 -0300

    Carga inicial da base de dados

--- src/main/java/br/com/erudio/apijavaservice/ApiJavaServiceApplication.java
+++ src/main/java/br/com/erudio/apijavaservice/ApiJavaServiceApplication.java
@@ -1,13 +1,48 @@
 package br.com.erudio.apijavaservice;
 
+import br.com.erudio.apijavaservice.domain.Chamado;
+import br.com.erudio.apijavaservice.domain.Cliente;
+import br.com.erudio.apijavaservice.domain.Tecnico;
+import br.com.erudio.apijavaservice.domain.enums.Perfil;
+import br.com.erudio.apijavaservice.domain.enums.Prioridade;
+import br.com.erudio.apijavaservice.domain.enums.Status;
+import br.com.erudio.apijavaservice.repositores.ChamadoRepository;
+import br.com.erudio.apijavaservice.repositores.ClienteRepository;
+import br.com.erudio.apijavaservice.repositores.TecnicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 @SpringBootApplication
-public class ApiJavaServiceApplication {
+public class ApiJavaServiceApplication implements CommandLineRunner {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
 
     public static void main(String[] args) {
         SpringApplication.run(ApiJavaServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Tecnico tecnico = new Tecnico(null, "jonathan", "42567079807", "jonathan@gmail.com", "123");
+        tecnico.addPerfil(Perfil.ADMIN);
+
+        Cliente cliente = new Cliente(null, "Pedro", "12345676543", "pedro@gmail.com", "123");
+
+        Chamado ch1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado-01", "Primeiro chamado", tecnico, cliente);
+
+        tecnicoRepository.saveAll(Arrays.asList(tecnico));
+        clienteRepository.saveAll(Arrays.asList(cliente));
+        chamadoRepository.saveAll(Arrays.asList(ch1));
+    }
 }
