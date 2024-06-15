7260815dec678b262d638798406e7db4580b496c
JonathanCarvalho039 <jonathan.carvalho@sptech.school>
2024-06-15 14:40:02 -0300

    Trabalhando o manipulador de exeÃssões

--- src/main/java/br/com/erudio/apijavaservice/resource/exeptions/ResourceExeptionHendler.java
+++ src/main/java/br/com/erudio/apijavaservice/resource/exeptions/ResourceExeptionHendler.java
@@ -1,2 +1,28 @@
-package br.com.erudio.apijavaservice.resource.exeptions;public class ResourceExeptionHendler {
+package br.com.erudio.apijavaservice.resource.exeptions;
+
+import br.com.erudio.apijavaservice.services.exeptions.ObjectNotFoundExeption;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExeptionHendler {
+
+    @ExceptionHandler(ObjectNotFoundExeption.class)
+    public ResponseEntity<StanderdError> objectNotFoundExeption(
+            ObjectNotFoundExeption ex,
+            HttpServletRequest request) {
+
+        StanderdError error = new StanderdError(
+                System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                "Object Not Found",
+                ex.getMessage(),
+                request.getRequestURI());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 }
--- src/main/java/br/com/erudio/apijavaservice/services/exeptions/ObjectNotFoundExeption.java
+++ src/main/java/br/com/erudio/apijavaservice/services/exeptions/ObjectNotFoundExeption.java
@@ -1,5 +1,13 @@
-package br.com.erudio.apijavaservice.services;
+package br.com.erudio.apijavaservice.services.exeptions;
 
-public class ObjectNotFoundExeption {
-    
+public class ObjectNotFoundExeption extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundExeption(String message) {
+        super(message);
+    }
+
+    public ObjectNotFoundExeption(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
