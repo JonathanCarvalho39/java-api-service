21922503f90321834a6209ec4b0329ad7412a10e
Jonathan Carvalho <142412881+JonathanCarvalho39@users.noreply.github.com>
2024-06-18 15:49:07 -0300

    Update api-service.sh

--- api-service.sh
+++ api-service.sh
@@ -1,79 +1,78 @@
 #!/bin/bash
 
-(
-    # Verificar se Docker está instalado
-    if ! which docker &> /dev/null
-    then
-        echo "-----------------------------------------------------"
-        echo "|               Instalando Docker...                |"
-        echo "-----------------------------------------------------"
+# Verificar se Docker está instalado
+if ! which docker &> /dev/null
+then
+    echo "-----------------------------------------------------"
+    echo "|               Instalando Docker...                |"
+    echo "-----------------------------------------------------"
 
-        # Atualizar pacotes existentes
-        sudo apt-get update
+    # Atualizar pacotes existentes
+    sudo apt-get update
 
-        # Remover versões antigas do Docker
-        sudo apt-get remove -y docker docker-engine docker.io containerd runc
+    # Remover versões antigas do Docker
+    sudo apt-get remove -y docker docker-engine docker.io containerd runc
 
-        # Instalar pacotes necessários para usar o repositório APT sobre HTTPS
-        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    # Instalar pacotes necessários para usar o repositório APT sobre HTTPS
+    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-        # Adicionar a chave GPG oficial do Docker
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    # Adicionar a chave GPG oficial do Docker
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-        # Adicionar o repositório do Docker às fontes do APT
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    # Adicionar o repositório do Docker às fontes do APT
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-        # Atualizar pacotes do APT novamente para incluir pacotes do Docker
-        sudo apt-get update
+    # Atualizar pacotes do APT novamente para incluir pacotes do Docker
+    sudo apt-get update
 
-        # Instalar Docker CE
-        sudo apt-get install -y docker-ce
+    # Instalar Docker CE
+    sudo apt-get install -y docker-ce
 
-        # Adicionar o usuário atual ao grupo Docker
-        sudo usermod -aG docker $USER
+    # Adicionar o usuário atual ao grupo Docker
+    sudo usermod -aG docker $USER
 
-        echo "Docker foi instalado com sucesso."
+    echo "Docker foi instalado com sucesso."
 
-        newgrp docker
+    newgrp docker
 
-        sudo systemctl restart docker
+    sudo systemctl restart docker
 
-    else
-        echo "Docker já está instalado."
+else
+    echo "Docker já está instalado."
+fi
+
+# Verificar se Docker Compose está instalado
+if ! which docker-compose &> /dev/null
+then
+    echo "-----------------------------------------------------"
+    echo "|           Instalando Docker Compose...            |"
+    echo "-----------------------------------------------------"
+
+    # Instalar Docker Compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+    # Aplicar permissões corretas ao binário do Docker Compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
+    # Criar um link simbólico para Docker Compose em /usr/bin se não existir
+    if [ ! -f /usr/bin/docker-compose ]; then
+        sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
     fi
+    
+    echo "Docker Compose foi instalado com sucesso."
+else
+    echo "Docker Compose já está instalado."
+fi
 
-    # Verificar se Docker Compose está instalado
-    if ! which docker-compose &> /dev/null
-    then
-        echo "-----------------------------------------------------"
-        echo "|           Instalando Docker Compose...            |"
-        echo "-----------------------------------------------------"
+# Verificar instalação
+echo "Docker version:"
+docker --version
 
-        # Instalar Docker Compose
-        sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo "Docker Compose version:"
+docker-compose --version
 
-        # Aplicar permissões corretas ao binário do Docker Compose
-        sudo chmod +x /usr/local/bin/docker-compose
-
-        # Criar um link simbólico para Docker Compose em /usr/bin se não existir
-        if [ ! -f /usr/bin/docker-compose ]; then
-            sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-        fi
-        
-        echo "Docker Compose foi instalado com sucesso."
-    else
-        echo "Docker Compose já está instalado."
-    fi
-
-    # Verificar instalação
-    echo "Docker version:"
-    docker --version
-
-    echo "Docker Compose version:"
-    docker-compose --version
-
-    # Criar o arquivo docker-compose.yml
-    cat <<EOF > docker-compose.yml
+# Criar o arquivo docker-compose.yml
+cat <<EOF > docker-compose.yml
 version: '3.8'
 
 services:
@@ -102,16 +101,11 @@ services:
     command: java -jar /api-server.jar
 EOF
 
-    # Rodar docker-compose em background
-    nohup docker-compose up -d &
+docker-compose up -d
+echo "Aplicação de Pé"
 
-    echo "Aplicação de Pé"
+# Esperar alguns segundos para garantir que os serviços estejam totalmente iniciados
+sleep 10
 
-    # Esperar alguns segundos para garantir que os serviços estejam totalmente iniciados
-    sleep 10
-
-    # Remover o arquivo docker-compose.yml
-    rm docker-compose.yml
-) &> install.log &
-
-echo "Script iniciado em background. Verifique 'install.log' para detalhes."
+# Remover o arquivo docker-compose.yml
+rm docker-compose.yml
